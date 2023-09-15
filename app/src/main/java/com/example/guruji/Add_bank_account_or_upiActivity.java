adc3def3560736dcf85e6bc63d58bcb04b4e0cb0
CHAHAT <bxrsushil@gmail.com>
2023-09-15 14:02:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/guruji/Add_bank_account_or_upiActivity.java
@@ -0,0 +1,14 @@
+package com.example.guruji;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_bank_account_or_upiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bank_account_or_upi);
+    }
+}
