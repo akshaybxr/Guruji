20d72130491079fe6efe8a012c1c4a1f7e85c846
CHAHAT <bxrsushil@gmail.com>
2023-09-29 00:43:23 +0530

    add fragment or navigation

--- app/src/main/java/com/example/guruji/DashboardActivity.java
+++ app/src/main/java/com/example/guruji/DashboardActivity.java
@@ -1,14 +1,24 @@
 package com.example.guruji;
 
+import static androidx.navigation.fragment.FragmentKt.findNavController;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.guruji.databinding.ActivityDashboardBinding;
 
 public class DashboardActivity extends AppCompatActivity {
+    ActivityDashboardBinding b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        b = ActivityDashboardBinding.inflate(getLayoutInflater());
+        View view = b.getRoot();
+        setContentView(view);
+        NavigationUI.setupWithNavController(b.bottomNavigation, findNavController(b.fcvHome.getFragment()));
     }
 }
--- app/src/main/java/com/example/guruji/SigninActivity.java
+++ app/src/main/java/com/example/guruji/SigninActivity.java
@@ -2,13 +2,27 @@ package com.example.guruji;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.guruji.databinding.ActivityDashboardBinding;
+import com.example.guruji.databinding.ActivitySigninBinding;
 
 public class SigninActivity extends AppCompatActivity {
+    ActivitySigninBinding b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        b = ActivitySigninBinding.inflate(getLayoutInflater());
+        View view = b.getRoot();
+        setContentView(view);
+        b.mbSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SigninActivity.this, DashboardActivity.class));
+            }
+        });
     }
 }
